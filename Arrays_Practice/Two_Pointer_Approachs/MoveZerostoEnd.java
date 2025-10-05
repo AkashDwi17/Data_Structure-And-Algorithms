9c02016da8f941211cbeafae9a3d801baef7bffe
Akash Dwivedi <akashdwi17@gmail.com>
2025-10-05 15:43:59 +0530

    Add File

---
+++ Arrays_Practice/Two_Pointer_Approachs/MoveZerostoEnd.java
@@ -0,0 +1,24 @@
+package Two_Pointer_Approachs;
+// Move Zeros to End
+// Input: [0,1,0,3,12]
+// Output: [1,3,12,0,0]
+public class MoveZerostoEnd {
+    public static void moveZeroToEnd (int arr[]){
+        int j = 0;
+        for (int i=0; i<arr.length; i++){
+            if (arr[i] != 0){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                j++;
+            }
+        }
+        for (int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {0,1,0,3,12};
+        moveZeroToEnd(arr);
+    }
+}
