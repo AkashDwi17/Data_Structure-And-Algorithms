a54da1740dc042fc9b334bb64486b395b589c45f
Akash Dwivedi <akashdwi17@gmail.com>
2025-10-06 16:18:47 +0530

    add

---
+++ Arrays_Practice/Two_Pointer_Approachs/PartitionBySign.java
@@ -0,0 +1,13 @@
+package Two_Pointer_Approachs;
+
+public class PartitionBySign {
+//     5.3 Partition by Sign
+//     Input: [0,-1,2,-3,5]
+//     Output: [-1,-3,2,5,0]
+    public static void (){
+        
+    }
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Arrays_Practice/Two_Pointer_Approachs/SlidingWindow/main.java
@@ -0,0 +1,7 @@
+package Two_Pointer_Approachs.SlidingWindow;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
