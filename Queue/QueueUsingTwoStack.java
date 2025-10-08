1ea5d421fbd44bcaa6486e5bb3ec3b330ea0ee7d
Akash Dwivedi <akashdwi17@gmail.com>
2025-10-08 22:46:26 +0530

    Queue

--- Queue/QueueUsingTwoStack.java
+++ Queue/QueueUsingTwoStack.java
@@ -1,33 +1,35 @@
 package Queue;
-
 import java.util.*;
-public class Queue_using_2_stack{
-    public static class Queue{
+
+public class QueueUsingTwoStack {
+    public static class Queue {
         static Stack <Integer> s1 = new Stack<>();
         static Stack <Integer> s2 = new Stack<>();
 
+        // Is Empty
         public static boolean isEmpty(){
             return s1.isEmpty();
         }
-        // add
-        public static void add (int data){
+
+        // Add
+        public static void add(int data){
             while (!s1.isEmpty()){
-                s2.push(s1.pop());
+                s2.push (s1.pop());
             }
-            s1.push(data);
+            s1.push (data);
             while (!s2.isEmpty()){
                 s1.push(s2.pop());
             }
         }
-        // remove
+
+        // Remove
         public static int remove(){
             if (isEmpty()){
                 return -1;
             }
-            return(s1.pop());
+            return s1.pop();
         }
-
-        // peek
+        // Peek
         public static int peek(){
             if (isEmpty()){
                 return -1;
@@ -35,7 +37,7 @@ public class Queue_using_2_stack{
             return s1.peek();
         }
     }
-    public static void main (String args[]){
+    public static void main(String[] args) {
         Queue q = new Queue();
         q.add(1);
         q.add(2);
