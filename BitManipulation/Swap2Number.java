da7ada08e12a27b3986bc5dafeeb40eab6212b0b
Akash Dwivedi <akashdwi17@gmail.com>
2025-11-15 23:11:52 +0530

    Reverse no.

---
+++ BitManipulation/Swap2Number.java
@@ -0,0 +1,15 @@
+package BitManipulation;
+
+public class Swap2Number {
+    public static void main (String args[]){
+        int x = 3;
+        int y = 5;
+
+        x = x^y;
+        y = x^y;
+        x = x^y;
+
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+    }
+}
--- _1Arrays_Practice/Main.java
+++ _1Arrays_Practice/Main.java
@@ -1,22 +1,19 @@
-    import java.util.*;
-    
-    public class SecondLargest {
+import java.util.*;
+public class Main {
     public static void main (String args[]){
+        int arr[] = {1,1,2,3,4,4,4};
+
         int largest = Integer.MIN_VALUE;
         int secLargest = Integer.MIN_VALUE;
 
-
-        int arr[] = {1, 2, 3, 4, 4, 4};
-
-        for (int i=0; i<arr.length; i++){
+        for (int i = 0; i<arr.length; i++){
             if (arr[i] > largest){
                 secLargest = largest;
                 largest = arr[i];
-            }else if (arr[i] < largest && arr[i] > largest){
+            }else if (arr[i] < largest && arr[i] > secLargest){
                 secLargest = arr[i];
             }
         }
         System.out.println(secLargest);
     }
 }
-
