68cd6b319fbb8af6bb02c741657af5569f7075de
Akash Dwivedi <akashdwi17@gmail.com>
2025-11-15 11:41:50 +0530

    String

---
+++ StringR/ConvertFirstLetterToUpperCaseLetter.java
@@ -0,0 +1,25 @@
+package StringR;
+
+public class ConvertFirstLetterToUpperCaseLetter {
+    public  static String convertUpperCase (String str){
+        StringBuilder sb = new StringBuilder("");
+        
+        char ch = Character.toUpperCase(str.charAt(0));
+        sb.append(ch);
+
+        for (int i=1; i<str.length(); i++){
+            if (i < str.length()-1 && str.charAt(i) == ' '){
+                sb.append (str.charAt(i));
+                i++;
+                sb.append (Character.toUpperCase(str.charAt(i)));
+            }else{
+                sb.append (str.charAt(i));
+            }   
+        }
+        return sb.toString();
+    }
+    public static void main(String[] args) {
+        String str = "hello, i am a student and u?";
+        System.out.println(convertUpperCase(str));
+    }
+}
---
+++ StringR/IsPalindrome.java
@@ -0,0 +1,17 @@
+package StringR;
+import java.util.*;
+
+public class IsPalindrome {
+    public static boolean isPalindrome (String str){
+        for (int i=0; i<str.length()/2; i++){
+            if (str.charAt(i) != str.charAt(str.length()-i-1)){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        String str = "racecar";
+        System.out.println(isPalindrome(str));
+    }
+}
