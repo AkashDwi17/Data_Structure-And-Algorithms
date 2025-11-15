28a66f0fe9dd6d73b323ddd972e362c5f31023c8
Akash Dwivedi <akashdwi17@gmail.com>
2025-11-15 22:49:29 +0530

    Bit Manipulation

--- BitManipulation/OddorEvenNumber.java
+++ BitManipulation/OddorEvenNumber.java
@@ -1,5 +1,17 @@
 package BitManipulation;
 
 public class OddorEvenNumber {
-    
+    public static void oddOrEven (int n){
+        int bitMask = 1;
+
+        if ((n&bitMask) == 0){
+            System.out.println(n + " Is Even Number");
+        }else{
+            System.out.println(n + " Is Odd Number");
+        }
+    }
+    public static void main(String[] args) {
+        oddOrEven(5);
+        oddOrEven(12);
+    }
 }
