446cdf205bbf46695183f98feb5d88e51312bce1
Akash Dwivedi <akashdwi17@gmail.com>
2025-11-25 02:55:25 +0530

    Find Pivot Index

--- Main.java
+++ Main.java
@@ -1,31 +1,22 @@
 import java.util.*;
 public class Main {
-    public static int countSubArrayDivByk (int arr[], int k){
-        HashMap <Integer, Integer> map = new HashMap<>();
-        map.put (0, 1);
-        int sum = 0;
-        int count = 0;
-
+    public static int findMidIdx (int arr[]){
+        int totalSum = 0;
         for (int i=0; i<arr.length; i++){
-            sum += arr[i];
-            int rem = sum % k;
-            if (rem < 0){
-                rem += k;
-            }
-            if (map.containsKey(rem)){
-                count += map.get(rem);
-            }
-            if (map.containsKey(rem)){
-                map.put (rem, map.get(rem)+1);
-            }
-            else{
-                map.put (rem, 1);
-            }
+            totalSum += arr[i];
         }
-        return count;
+        int leftSum = 0;
+        for (int i=0; i<arr.length-1; i++){
+            int rightSum = totalSum - leftSum - arr[i];
+            if (leftSum == rightSum){
+                return i;
+            }
+            leftSum += arr[i];
+        }
+        return -1;
     }
-    public static void main (String args[]){
-        int arr[] = {4,5,0,-2,-3,1};
-        System.out.println(countSubArrayDivByk(arr, 5));
+    public static void main (String  args[]){
+        int arr[] = {1,7,3,6,5,6};
+        System.out.println(findMidIdx(arr));
     }
 }
