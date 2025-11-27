70b8cba577a4bd49ccfa5da4268c5c61db5097ad
Akash Dwivedi <akashdwi17@gmail.com>
2025-11-27 19:44:22 +0530

    Top view of tree

--- Main.java
+++ Main.java
@@ -12,58 +12,72 @@ public class Main {
         }
     }
 
-    public static boolean isIdentical (Node root, Node subRoot){
-        if (root == null && subRoot == null ){
-            return true;
+    public static class Info  {
+        Node node;
+        int hd;
+        public Info (Node node, int hd){
+            this.node =  node;
+            this.hd = hd;
         }
-        if (root == null || subRoot == null || root.data != subRoot.data){
-            return false;
+    }
+    public static void topView (Node root){
+        Queue <Info> q = new LinkedList<>();
+        HashMap <Integer, Node> map = new HashMap<>();
+
+        int min = 0;
+        int max = 0;
+
+        q.add (new Info(root, 0));
+        q.add (null);
+
+        while (!q.isEmpty()){
+            Info curr = q.remove();
+            if (curr == null){
+                if (q.isEmpty()){
+                    break;
+                }
+                q.add(null);
+            }else{
+                if (!map.containsKey(curr.hd)){
+                    map.put (curr.hd, curr.node);
+                }
+                if (curr.node.left != null){
+                    q.add(new Info(curr.node.left, curr.hd-1));
+                    min = Math.min (min, curr.hd-1);
+                }
+
+                if (curr.node.right != null){
+                    q.add(new Info(curr.node.right, curr.hd+1));
+                    max = Math.max(max, curr.hd+1);
+                }
+            }
         }
-        if (!isIdentical(root.left, subRoot.left)){
-            return false;
+        for (int i=min; i<=max; i++){
+            System.out.print(map.get(i).data+" ");
         }
-        if (!isIdentical(root.right, subRoot.right)){
-            return false;
-        }
-        return true;
+        System.out.println();
     }
 
-    public static boolean isSubtree (Node root, Node subRoot){
-        if (root == null){
-            return false;
-        }
-        if (isIdentical(root, subRoot)){
-            return true;
-        }
-        return isSubtree(root.left, subRoot) &&
-                isSubtree(root.right, subRoot);
-    }
     public static void main (String args[]){
-         // Manually creating the same tree:
-        //         1
-        //       /   \
-        //      2     3
-        //     / \   / \
-        //    4  5  6  7
-        Node root = new Node (1);
-        root.left = new Node (2);
+
+
+
+        /*
+         1
+       /   \
+      2     3
+     / \   / \
+    4   5 6   7
+        
+        */
+         Node root = new Node(1);
+        root.left = new Node(2);
         root.left.left = new Node (4);
         root.left.right = new Node (5);
-
         root.right = new Node (3);
         root.right.left = new Node (6);
         root.right.right = new Node (7);
 
-        // Manually creating the same tree:
-        //      2     
-        //     / \   
-        //    4  5  
-        // Subtree
-
-        Node subRoot = new Node (2);
-        subRoot.left = new Node (4);
-        subRoot.right = new Node (4);
-
-        System.out.println("Is Subtree: " + isSubtree(root, subRoot)); // true
+        topView(root);
     }
 }
