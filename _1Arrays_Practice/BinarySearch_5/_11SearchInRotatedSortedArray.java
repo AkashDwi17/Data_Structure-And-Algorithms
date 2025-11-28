995911bc3cc834e477f3ff625e76a557761c4076
Akash Dwivedi <akashdwi17@gmail.com>
2025-11-28 17:59:04 +0530

    Search in sorted and rotated array

---
+++ _1Arrays_Practice/BinarySearch_5/_11SearchInRotatedSortedArray.java
@@ -0,0 +1,57 @@
+package BinarySearch_5;
+
+// Problem 11 — Search in Rotated Sorted Array (LeetCode 33)
+
+// Companies: Amazon, Google
+// Input: arr = [4,5,6,7,0,1,2], target = 0
+// Output: 4
+
+
+public class _11SearchInRotatedSortedArray {
+    public static int search (int arr[], int si, int ei, int key){
+        if (si > ei){
+            return -1;
+        }
+        int mid = si + (ei-si)/2;
+
+        if (arr[mid] == key){
+            return mid;
+        }
+        if (arr[si] <= arr[mid]){
+            if (arr[si] <= key && key <= arr[mid]){
+                return search(arr, si, mid-1, key);
+            }else {
+                return search(arr, mid+1, ei, key);
+            }
+        }else{
+            if (key >= arr[mid] && key <= arr[ei]){
+                return search(arr, mid+1, ei, key);
+            }else{
+                return search(arr, si, mid-1, key);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {4,5,6,7,0,1,2};
+        System.out.println(search(arr, 0, arr.length-1, 4));
+        System.out.println();
+
+        System.out.println(search(arr, 0, arr.length-1, 5));
+        System.out.println();
+
+        System.out.println(search(arr, 0, arr.length-1, 6));
+        System.out.println();
+
+        System.out.println(search(arr, 0, arr.length-1, 7));
+        System.out.println();
+
+        System.out.println(search(arr, 0, arr.length-1, 0));
+        System.out.println();
+
+        System.out.println(search(arr, 0, arr.length-1, 1));
+        System.out.println();
+
+        System.out.println(search(arr, 0, arr.length-1, 2));
+        System.out.println();
+    }
+}
