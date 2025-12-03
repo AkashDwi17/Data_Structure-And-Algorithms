05e388eb7b4f64339670dce18fcc157377f6a494
Akash Dwivedi <akashdwi17@gmail.com>
2025-12-03 13:07:23 +0530

    Sorting Algorithm

--- Main.java
+++ Main.java
@@ -1,68 +1,80 @@
 import java.util.*;
 public class Main{
+
     public static void print (int arr[]){
         for (int i=0; i<arr.length; i++){
             System.out.print(arr[i]+" ");
         }
         System.out.println ();
     }
+    public static void mergeSort (int arr[], int si, int ei){
+        if (si >= ei){
+            return;
+        }
+        int mid = si+(ei-si)/2;
+        mergeSort (arr, si, mid);
+        mergeSort (arr, mid+1, ei);
+        merge (arr, si, mid, ei);
+    }
 
-    // Bubble Sort
-    // Large Elements comes to the end of array by swapping the adjacent elements to the end of array
+    public static void merge (int arr[], int si, int mid, int ei){
+        int temp[] = new int [ei-si+1];
+        int i = si;
+        int j = mid+1;
+        int k = 0;
 
-    public static void bubbleSort (int arr[]){
-
-        for (int i = 0; i<arr.length-1; i++){
-            for (int j=0; j<arr.length-i-1; j++){
-                if (arr[j] > arr[j+1]){
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
-                }
+        while (i <= mid && j <= ei){
+            if (arr[i] < arr[j]){
+                temp[k] = arr[i];
+                i++;
             }
+            else{
+                temp[k] = arr[j];
+                    j++;
+            }
+            k++;
+        }
+        while (i <= mid){
+            temp[k++] = arr[i++];
+        }
+        while (j <= ei){
+            temp[k++] = arr[j++];
+        }
+        for (k=0, i=si; k<temp.length; k++, i++){
+            arr[i] = temp[k];
         }
     }
 
-    // Selection Sort 
-    // pick an elements from the unsorted part and put it at the begning
-
-    public static void selectionSort (int arr[]){
-        for (int i=0; i<arr.length-1; i++){
-            int minPos = i;
-            for (int j=i+1; j<arr.length; j++){
-                if (arr[minPos] > arr[j]){
-                    minPos = j;
-                }
-            }
-            int temp = arr[minPos];
-            arr[minPos] = arr[i];
-            arr[i] = temp;
-        } 
-    }
-
-    // Insertion Sort
-
-    // Pick an element from the unsorted part and put it at its correct position
-    public static void insertionSort (int arr[]){
-        for (int i=0; i<arr.length; i++){
-            int curr = arr[i];
-            int prev = i-1;
-            while (prev >= 0 && arr[prev] > curr){
-                arr[prev+1] = arr[prev];
-                prev --;
-            }
-            arr[prev+1] = curr;
+    public static void quickSort (int arr[], int si, int ei){
+        if (si >= ei){
+            return;
         }
+        int pIdx = partition (arr, si, ei);
+        quickSort(arr, si, pIdx-1);
+        quickSort(arr, pIdx+1, ei);
     }
 
-
-    public  static void main (String args[]){
+    public static int partition (int arr[], int si, int ei){
+        int piviot = arr[ei];
+        int i = si-1;
+        for (int j=si; j<ei; j++){
+            if (arr[j] < piviot){
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        i++;
+        int temp = arr[i];
+        arr[i] = arr[ei];
+        arr[ei] = temp;
+        return i;
+    }
+    public static void main(String args[]){
         int arr[] = {5,4,1,3,2};
-        bubbleSort(arr);
         print(arr);
-        selectionSort(arr);
-        print(arr);
-        insertionSort(arr);
+        quickSort(arr, 0, arr.length-1);
         print(arr);
     }
 }
