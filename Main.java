bc68d665351f90d109b003a1b2548c5a86353cff
Akash Dwivedi <akashdwi17@gmail.com>
2025-12-04 18:00:13 +0530

    LL

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class LinkedList {
+public class Main {
     public static class Node {
         int data;
         Node next;
@@ -9,11 +9,11 @@ public class LinkedList {
             this.next = null;
         }
     }
-    public  Node head;
-    public  Node tail;
+    public static Node head;
+    public static Node tail;
 
     // Add First
-    public  void addFirst (int data){
+    public static void addFirst (int data){
         Node newNode = new Node (data);
         if (head == null){
             head = tail = newNode;
@@ -25,7 +25,7 @@ public class LinkedList {
 
 
     // Add Last 
-    public void addLast (int data){
+    public static void addLast (int data){
         Node newNode = new Node (data);
         if (head == null){
             head = tail = newNode;
@@ -36,7 +36,7 @@ public class LinkedList {
     }
 
     // Print
-    public void print (){
+    public static void print (){
         if (head == null){
             System.out.print("null ");
         }
@@ -47,13 +47,15 @@ public class LinkedList {
         }
         System.out.print("null ");
     }
+
+    
     public static void main (String args[]){
-        LinkedList ll = new LinkedList ();
-        ll.addLast(1);
-        ll.addLast(2);
-        ll.addLast(3);
-        ll.addLast(4);
-        ll.addLast(5);
-        ll.print();
+        // LinkedList ll = new LinkedList ();
+        addLast(1);
+        addLast(2);
+        addLast(3);
+        addLast(4);
+        addLast(5);
+        print();
     }
 }
