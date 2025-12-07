8aa47602cb010a9ab19156745e02598ee2d3af7c
Akash Dwivedi <akashdwi17@gmail.com>
2025-12-07 16:46:52 +0530

    _3SumClosest

--- _1Arrays_Practice/SORTINGBASEDPROBLEMS_6/_3SumClosest.java
+++ _1Arrays_Practice/SORTINGBASEDPROBLEMS_6/_3SumClosest.java
@@ -7,6 +7,39 @@ package SORTINGBASEDPROBLEMS_6;
 // Input: arr = [-1,2,1,-4], target=1
 // Output: 2
 
+import java.util.*;
+
 public class _3SumClosest {
-    
+
+    public static int threeSumClosest(int[] arr, int target) {
+        Arrays.sort(arr);
+        int closest = arr[0] + arr[1] + arr[2]; // initialize with first triplet
+
+        for (int i = 0; i < arr.length - 2; i++) {
+            int si = i + 1;
+            int ei = arr.length - 1;
+
+            while (si < ei) {
+                int sum = arr[i] + arr[si] + arr[ei];
+
+                // update if closer
+                if (Math.abs(target - sum) < Math.abs(target - closest)) {
+                    closest = sum;
+                }
+
+                // move pointers
+                if (sum < target) si++;
+                else ei--;
+            }
+        }
+        return closest;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {-1, 2, 1, -4};
+        int target = 1;
+
+        System.out.println(threeSumClosest(arr, target));  // Output: 2
+    }
 }
+
