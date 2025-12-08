a81cc2ad48c3891e24eccadf223537bd4790faab
Akash Dwivedi <akashdwi17@gmail.com>
2025-12-08 09:56:21 +0530

    Prefix Sum Optimized Solution

--- _1Arrays_Practice/PrefixSumSuffixSum/ConstructPrefixSumArray.java
+++ _1Arrays_Practice/PrefixSumSuffixSum/ConstructPrefixSumArray.java
@@ -8,6 +8,9 @@ import java.util.*;
 // Output: prefix = [1, 3, 6, 10]
 // Idea: prefix[i] = prefix[i-1] + arr[i]
 
+// Optimized code below
+
+// ITS TIME COMPLEXITY IS O(n) and SPACE COMPLEXITY IS O(n)
 public class ConstructPrefixSumArray {
     public static List<Integer> constructPrefixSumArray (int arr[]){
         ArrayList <Integer> pref = new ArrayList<>();
@@ -18,6 +21,21 @@ public class ConstructPrefixSumArray {
         }
         return pref;
     }
+
+    // ITS TIME COMPLEXITY IS O(n) and SPACE COMPLEXITY IS O(1)
+    public static void pref1(int arr[]){
+        int k = 0;
+        int sum = 0;
+        for (int i=0; i<arr.length; i++){
+            sum += arr[i];
+            arr[i] = sum;
+        }
+        for (int i=0; i<arr.length; i++){
+            System.out.print (arr[i]+" ");
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
         int arr[] = {1, 2, 3, 4};
         System.out.println(constructPrefixSumArray(arr));
