1f2826451560b1456ea34bf3bdbcd7ddfe335d39
Akash Dwivedi <akashdwi17@gmail.com>
2025-12-08 10:47:03 +0530

    Find Mid Index

--- Main.java
+++ Main.java
@@ -1,30 +1,22 @@
 import java.util.*;
-public class Main{
-    public static int countSubArray (int arr[], int k){
-        HashMap<Integer, Integer> map = new HashMap<>();
-        map.put (0, 1);
-        int sum = 0;
-        int count = 0;
+public class Main {
+    public static int findMidIdx (int arr[]){
+        int totalSum = 0;
         for (int i=0; i<arr.length; i++){
-            sum += arr[i];
-            int rem = sum % k;
-            if (rem < 0){
-                rem += k;
-            }
-            if (map.containsKey (rem)){
-                count = count + map.get(rem);
-            }
-            if (map.containsKey (rem)){
-                map.put (rem, map.get(rem)+1);
-            }else{
-                map.put (rem, 1);
-            }
+            totalSum += arr[i];
         }
-        return count;
+        int leftSum = 0;
+        for (int i=0; i<arr.length-1; i++){
+            int rightSum = totalSum-leftSum - arr[i];
+            if (leftSum == rightSum){
+                return i;
+            }
+            leftSum += arr[i];
+        }
+        return -1;
     }
     public static void main (String args[]){
-        int arr[] = {4,5,0,-2,-3,1};
-        int k = 5;
-        System.out.println(countSubArray(arr, k));
+        int arr[] = {1,7,3,6,5,6};
+        System.out.println(findMidIdx(arr));
     }
 }
