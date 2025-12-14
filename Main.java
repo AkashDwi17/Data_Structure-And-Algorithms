59bc3c069f93ee346be3ec9fd3570629a45fb5f4
Akash Dwivedi <akashdwi17@gmail.com>
2025-12-14 15:57:34 +0530

    find Minimum distence between 2 nodes

--- Main.java
+++ Main.java
@@ -229,16 +229,62 @@ public class Main {
         }
         return max+1;
     }
-    
+    // All setup to find Minimum distence between 2 nodes
+    public static Node lca2 (Node root, int n1, int n2){
+        if  (root == null || root.data == n1 || root.data == n2){
+            return root;
+        }
+        Node leftInfo = lca2 (root.left, n1, n2);
+        Node rightInfo = lca2 (root.right, n1, n2);
+        if (leftInfo == null){
+            return rightInfo;
+        }
+        if (rightInfo == null){
+            return leftInfo;
+        }
+        return root;
+    }
+
+    public static int lcaDist (Node root, int n){
+        if (root == null){
+            return -1;
+        }
+        if (root.data == n){
+            return 0;
+        }
+        int leftInfo = lcaDist(root.left, n);
+        int rightInfo = lcaDist(root.right, n);
+
+        if (leftInfo == -1 && rightInfo == -1){
+            return -1;
+        }
+        else if (leftInfo == -1){
+            return rightInfo+1;
+        }
+        else {
+            return leftInfo+1;
+        }
+    }
+
+    public static int minDistBetNode (Node root, int n1, int n2){
+        Node lca = lca2(root, n1, n2);
+        int dist1 = lcaDist(lca, n1);
+        int dist2 = lcaDist(lca, n2);
+
+        return dist1 + dist2;
+    }
     public static void main (String args[]){
+
         Node root = new Node(1);
         root.left = new Node(2);
-        root.left.left = new Node (4);
-        root.left.right = new Node (5);
-        root.right = new Node (3);
-        root.right.left = new Node (6);
-        root.right.right = new Node (7);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
 
-        topView(root);
+        int n1 = 4; 
+        int n2 = 6;
+        System.out.println(minDistBetNode(root, n1, n2));
     }
 }
