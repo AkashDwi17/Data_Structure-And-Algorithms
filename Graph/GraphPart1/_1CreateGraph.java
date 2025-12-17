6a2b591e3e40f9effccb5acf1b105dfed2704389
Akash Dwivedi <akashdwi17@gmail.com>
2025-12-17 10:01:18 +0530

    Create Graph Using Adjency List

--- Graph/GraphPart1/_1CreateGraph.java
+++ Graph/GraphPart1/_1CreateGraph.java
@@ -6,30 +6,45 @@ public class _1CreateGraph {
         int src;
         int dest;
         int wt;
-        public Edge (int src, int dest, int wt){
-            this.src = src;
-            this.dest = dest;
+        public Edge (int s, int d, int wt){
+            this.src = s;
+            this.dest = d;
             this.wt = wt;
         }
-    } 
+    }
 
     public static void createGraph (ArrayList<Edge> graph[]){
         for (int i=0; i<graph.length; i++){
             graph[i] = new ArrayList<>();
         }
-        graph[0].add (new Edge(0, 1, 1));
-        graph[0].add (new Edge(0, 2, 1));
-        graph[1].add (new Edge(1, 0, 1));
-        graph[1].add (new Edge(1, 3, 1));
+        graph[0].add (new Edge(0,1,1));
 
-        for (int i=0; i<graph[1].size(); i++){
-            Edge e = graph[1].get(i);
-            System.out.println (e.dest);
+        graph[1].add (new Edge(1,0,1));
+        graph[1].add (new Edge(1,2,1));
+        graph[1].add (new Edge(1,3,1));
+
+        graph[2].add (new Edge(2,1,1));
+        graph[2].add (new Edge(2,3,1));
+        graph[2].add (new Edge(2,4,1));
+
+        graph[3].add (new Edge(3,1,1));
+        graph[3].add (new Edge(3,2,1));
+
+        graph[4].add (new Edge(4,3,1));
+
+        for (int i=0; i<graph[2].size(); i++){
+            Edge e = graph[2].get(i);
+            System.out.print(e.dest+(" "));
         }
+        System.out.println ();
     }
+
     public static void main (String args[]){
         int v = 5;
-        ArrayList<Edge> graph [] = new ArrayList[v];
+        ArrayList <Edge> graph[] = new ArrayList[v];
         createGraph(graph);
     }
-}
+} 
+
+    
+
