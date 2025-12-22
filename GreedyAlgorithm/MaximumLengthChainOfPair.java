4b455cc7b7fed67b4ae12b9c0376ece8970983dd
Akash Dwivedi <akashdwi17@gmail.com>
2025-12-22 21:34:17 +0530

    Maximum Length Chain Of Pair

--- GreedyAlgorithm/MaximumLengthChainOfPair.java
+++ GreedyAlgorithm/MaximumLengthChainOfPair.java
@@ -6,13 +6,13 @@ public class MaximumLengthChainOfPair {
         int pairs[][] = {{5,24}, {39,60}, {5,28}, {27,40}, {50,90}};
 
         Arrays.sort (pairs, Comparator.comparingDouble (o->o[1]));
-        int chainLen = 1;
-        int lastEnd = pairs[0][1];
 
+        int chainEnd = pairs[0][1];
+        int chainLen = 1;
         for (int i=1; i<pairs.length; i++){
-            if (pairs[i][0] > chainLen){
+            if (chainEnd <= pairs[i][0]){
                 chainLen ++;
-                lastEnd = pairs[i][1];
+                chainEnd = pairs[i][1];
             }
         }
         System.out.println("Maximum Chain Length: " + chainLen);
