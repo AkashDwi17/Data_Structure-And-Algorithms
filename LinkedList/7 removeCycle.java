e0b9039ed9441251d736e286ed77b13daf03dc10
Akash Dwivedi <akashdwi17@gmail.com>
2026-01-22 19:35:57 +0530

    Remove cycle in LL

--- LinkedList/7 removeCycle.java
+++ LinkedList/7 removeCycle.java
@@ -3,43 +3,69 @@ package LinkedList;
 
 // remove cycle from LinkedList
 
-public static void removeCycle(){
-    Node slow = head;
-    Node fast = head;
-    boolean isCycle = false;
-    while (fast != null && fast.next != null){
-        slow = slow.next;
-        fast = fast.next.next;
-        if (fast == slow){
-            isCycle = true;
-            break;
-        }
-    }
-
-    if (isCycle == false){
-        System.out.println("No cycle is detected");
-        return;
-    }
-    slow = head;
-    Node prev = null;
-    while (slow != fast){
-        prev = fast;
-        slow = slow.next;
-        fast = fast.next;
-    }
-    prev.next = null;
-}
-    public static void main (String args[]){
-        head = new Node(1);
-        Node temp = new Node(5);
-        head.next = temp;
-        head.next.next = new Node(3);
-        head.next.next.next = temp;
+ public static void removeCycle (){
+        Node slow = head;
+        Node fast = head;
+        boolean isCycle = false;
         
-        System.out.println(isCycle());
+        while (fast!= null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast){
+                isCycle = true;
+                break;
+            }
+        }
+        if (isCycle == false){
+            System.out.println("Cycle Doesen't Exists!");
+            return;
+        }
 
+        Node prev = null;
+        slow = head;
+
+
+//         🔁 Example 1: Cycle starts at head
+// 1 → 2 → 3
+// ↑       ↓
+// ← ← ← ← ←
+
+        // Case 1
+        if (slow == fast){
+            while (fast.next != slow){
+                fast = fast.next;
+            }
+            fast.next = null;
+            return;
+        }
+
+
+
+        while (slow != fast){
+            prev = fast;
+            slow = slow.next;
+            fast = fast.next;
+        }
+        prev.next = null;
+    }
+    
+    public static void main (String args[]){
+        // addLast(1);
+        // addLast(2);
+        // addLast(2);
+        // addLast(2);
+        // print();
+        // System.out.print (isPalindrome());
+        
+
+        head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = head;
+        System.out.println(isCycle());
+        System.out.println();
         removeCycle();
-         System.out.println(isCycle());
+        System.out.println(isCycle());
 
     }
 }
