efefae3bdaa580c413b436a4184e6e14aeacbab5
Akash Dwivedi <akashdwi17@gmail.com>
2026-01-23 09:16:36 +0530

    Find Dublicate Parenthesis

--- Stack/DublicateParenthesis.java
+++ Stack/DublicateParenthesis.java
@@ -3,29 +3,27 @@ import java.util.*;
 public class DublicateParenthesis {
     public static boolean dublicateParenthesis (String str){
         Stack <Character> s = new Stack<>();
-
         for (int i=0; i<str.length(); i++){
             char ch = str.charAt(i);
             if (ch == ')'){
                 int count = 0;
-                while (s.peek() != '('){
-                    count ++;
+                while (!s.isEmpty() && s.peek() != '('){
                     s.pop();
+                    count ++;
                 }
                 if (count < 1){
                     return true;
-                }
-                else{
+                }else{
                     s.pop();
                 }
             }else{
-                s.push(ch);
+                s.push (ch);
             }
         }
         return false;
     }
-    public static void main(String[] args) {
-        String str = "((a+(n(l()p))u))";
-        System.out.println(dublicateParenthesis(str));
+    public static void main (String args[]){
+        String str = "((a+b)*(c+d))";
+        System.out.print (dublicateParenthesis(str));
     }
 }
