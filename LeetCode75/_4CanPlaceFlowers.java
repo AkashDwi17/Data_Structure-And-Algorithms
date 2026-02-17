991581d651c3d53eb726a91879d77cae06159085
Akash Dwivedi <akashdwi17@gmail.com>
2026-02-18 00:55:48 +0530

    leetcode

--- LeetCode75/_4CanPlaceFlowers.java
+++ LeetCode75/_4CanPlaceFlowers.java
@@ -1,5 +1,49 @@
 package LeetCode75;
 
+// Example 1:
+
+// Input: flowerbed = [1,0,0,0,1], n = 1
+// Output: true
+// Example 2:
+
+// Input: flowerbed = [1,0,0,0,1], n = 2
+// Output: false
+
+
 public class _4CanPlaceFlowers {
-    
+    public static boolean canPlaceFlowers(int arr[], int k) {
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] == 0) {
+
+                int prev = (i == 0) ? 0 : arr[i - 1];
+                int next = (i == arr.length - 1) ? 0 : arr[i + 1];
+
+                if (prev == 0 && next == 0) {
+                    arr[i] = 1;
+                    k--;
+
+                    if (k == 0) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return k <= 0;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1,0,0,0,1};
+        int k = 2;
+        System.out.println(canPlaceFlowers(arr, k));
+    }
 }
+
+
+
+
+// You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
+
+// Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return true if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule and false otherwise.
---
+++ LeetCode75/_5ReverseVowelsOfAString.java
@@ -0,0 +1,44 @@
+package LeetCode75;
+
+// Given a string s, reverse only all the vowels in the string and return it.
+// The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
+// Example 1:
+// Input: s = "IceCreAm"
+// Output: "AceCreIm"
+
+// Explanation:
+// The vowels in s are ['I', 'e', 'e', 'A']. On reversing the vowels, s becomes "AceCreIm".
+// Example 2:
+// Input: s = "leetcode"
+// Output: "leotcede"
+
+public class _5ReverseVowelsOfAString {
+    public static boolean isVowel (char ch){
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
+    }
+    public static String reverseVowelsOfAString (String str){
+        char arr[] = str.toCharArray();
+        int left = 0;
+        int right = arr.length-1;
+
+        while (left < right){
+            if (!isVowel(arr[left])){
+                left ++;
+            }
+            else if (!isVowel(arr[right])){
+                right --;
+            }else{
+                char temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+
+                left ++;
+                right --;
+            }
+        }
+        return new String (arr);
+    }
+    public static void main(String[] args) {
+        
+    }
+}
